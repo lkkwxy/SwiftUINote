f7cb36ff33221d07224b6f9a4bce8f44403921f3
lkkwxy <1403705809@qq.com>
2020-11-10 08:07:25 +0800

    完成面板的header

--- PokeMaster/PokeMaster/View/PokemonInfoPanel.swift
+++ PokeMaster/PokeMaster/View/PokemonInfoPanel.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 struct PokemonInfoPanel: View {
-    let pokemonMode: PokemonViewModel = PokemonViewModel.sample(id: 34)
+    let pokemonMode: PokemonViewModel = PokemonViewModel.sample(id: 1)
     let abilitys : [AbilityViewModel]
     
     init() {
@@ -22,6 +22,7 @@ struct PokemonInfoPanel: View {
     }
     var body: some View {
         VStack {
+//            Spacer()
             topIndicator
             Header(pokemonMode: pokemonMode)
         }
@@ -33,9 +34,65 @@ extension PokemonInfoPanel {
     
     struct Header: View {
         let pokemonMode: PokemonViewModel
+        var spliteLine: some View {
+            RoundedRectangle(cornerRadius: 0)
+                .frame(width: 1, height: 44)
+                .opacity(0.1)
+        }
         var body: some View {
-            HStack {
+            HStack(spacing: 18) {
                 Image("Pokemon-\(pokemonMode.id)")
+                    .resizable()
+                    .frame(width: 68, height: 68)
+                VStack {
+                    Text("\(pokemonMode.name)")
+                        .foregroundColor(pokemonMode.color)
+                        .font(.system(size: 22, weight: .bold))
+                    Text("\(pokemonMode.nameEN)")
+                        .foregroundColor(pokemonMode.color)
+                        .font(.system(size: 13, weight: .bold))
+                    Text("\(pokemonMode.genus)")
+                        .foregroundColor(Color(red: 0.3, green: 0.3, blue: 0.3, opacity: 1))
+                        .font(.system(size: 13, weight: .bold))
+                        .padding(.top, 10)
+                }
+                spliteLine
+                VStack(spacing: 12) {
+                    VStack(alignment: .leading, spacing: 2) {
+                        HStack {
+                            Text("身高")
+                                .font(.system(size : 11))
+                                .foregroundColor(.gray)
+                            Text("\(pokemonMode.height)")
+                                .font(.system(size : 11))
+                                .foregroundColor(pokemonMode.color)
+                        }
+                        HStack {
+                            Text("体重")
+                                .font(.system(size : 11))
+                                .foregroundColor(.gray)
+                            Text("\(pokemonMode.weight)")
+                                .font(.system(size : 11))
+                                .foregroundColor(pokemonMode.color)
+                        }
+                    }
+                    
+                    HStack {
+                        ForEach(pokemonMode.types) { (type) in
+                            Text("\(type.name)")
+                                .font(.system(size : 10))
+                                .foregroundColor(.white)
+                                .frame(width: 36, height: 14)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 7)
+                                        .foregroundColor(type.color)
+                                )
+                        }
+                        
+                    }
+                    
+                }
+                
             }
         }
         
